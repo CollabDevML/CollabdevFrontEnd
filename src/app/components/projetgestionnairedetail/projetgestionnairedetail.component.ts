073771be8ec3d7837ee5f23613f2ed0b20e07323
jimlecracken14 <jimlecracken13@gmail.com>
2025-08-08 15:03:16 +0000

    feature: visualisé un projet start

---
+++ src/app/components/projetgestionnairedetail/projetgestionnairedetail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projetgestionnairedetail',
+  imports: [],
+  templateUrl: './projetgestionnairedetail.component.html',
+  styleUrl: './projetgestionnairedetail.component.css'
+})
+export class ProjetgestionnairedetailComponent {
+
+}
