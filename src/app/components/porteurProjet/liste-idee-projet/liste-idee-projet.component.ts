7359c40c22891fcfd15cee9c23e93b68e96e56d6
Mohamed Diop <mohdiopcode@gmail.com>
2025-08-11 09:09:34 +0000

    Supprime la redondance de l'importation

--- src/app/components/porteurProjet/liste-idee-projet/liste-idee-projet.component.ts
+++ src/app/components/porteurProjet/liste-idee-projet/liste-idee-projet.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { SideBarComponent } from '../../UI/side-bar/side-bar.component';
 import { HeaderComponent } from '../../UI/header/header.component';
 import { RecherchebarreComponent } from '../../UI/recherchebarre/recherchebarre.component';
-import { CommonModule } from '@angular/common';
 import { ResponseIdeeProjet2 } from '../../../models/ideeprojet/response-idee-projet2';
 import { IdeeprojetService } from '../../../services/ideeprojet/ideeprojet.service';
 import { CommonModule, DatePipe } from '@angular/common';
@@ -10,9 +9,6 @@ import { CommonModule, DatePipe } from '@angular/common';
 @Component({
   selector: 'app-liste-idee-projet',
   imports: [
-    SideBarComponent,
-    RecherchebarreComponent,
-    DatePipe,
     CommonModule
   ],
   templateUrl: './liste-idee-projet.component.html',
