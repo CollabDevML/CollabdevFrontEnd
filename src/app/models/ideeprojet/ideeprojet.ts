18d0377c9a5dd4def9248d94d58738a6c1370632
Kadiatou <kadiatou2021tall@gmail.com>
2025-08-14 17:42:49 +0000

    correction conflit

--- src/app/models/ideeprojet/ideeprojet.ts
+++ src/app/models/ideeprojet/ideeprojet.ts
@@ -1,5 +1,4 @@
 export class Ideeprojet {
-<<<<<<< HEAD
 readonly id!:number;
 public titre!:string;
 public description!:string;
@@ -13,20 +12,4 @@ dateCommentaire:Date;
  utilisateur:{prenom:string,nom:string,role:string}
 id:number}[];
 
-=======
-  readonly id!: number;
-  public titre!: string;
-  public description!: string;
-  public domaine!: string;
-  public uriCDC!: string;
-  public nombreSoutien!: number;
-  public datePublication!: Date;
-  public utilisateur!: { prenom: string; nom: string };
-  public commentaireIdeeProjets!: {
-    contenu: string;
-    dateCommentaire: Date;
-    utilisateur: { prenom: string; nom: string; role: string };
-    id: number;
-  }[];
->>>>>>> c96d192950db4f93533317dd7842b87ec1540190
 }
