de7737f54ed9247864dd84ffe18abfe35860cf18
Abdoul03 <samabdoul03@gmail.com>
2025-08-14 14:48:49 +0000

    fusion du tag

--- src/app/components/gestionnaire/formulaire/formulaire-projet/formulaire-projet.component.ts
+++ src/app/components/gestionnaire/formulaire/formulaire-projet/formulaire-projet.component.ts
@@ -1,67 +1,80 @@
 import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, NgModule, OnInit } from '@angular/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  NgModule,
+  OnInit,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { GestionnaireDataService } from '../../../../services/gestionnaire/gestionnaire-data.service';
 import { projet } from '../../../../models/projet/projet';
 import { Enumerations } from '../../../../models/enums/enums';
 import { ToastrService } from 'ngx-toastr';
-import { NgxSpinnerService, NgxSpinnerComponent, NgxSpinnerModule } from 'ngx-spinner';
+import {
+  NgxSpinnerService,
+  NgxSpinnerComponent,
+  NgxSpinnerModule,
+} from 'ngx-spinner';
 
 @Component({
   selector: 'app-formulaire-projet',
-  imports: [
-    CommonModule,
-    FormsModule,
-    NgxSpinnerComponent,
-    NgxSpinnerModule
-],
+  imports: [CommonModule, FormsModule, NgxSpinnerComponent, NgxSpinnerModule],
   templateUrl: './formulaire-projet.component.html',
   styleUrl: './formulaire-projet.component.css',
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class FormulaireProjetComponent implements OnInit {
-  projetIdee: projet = new projet;
+  projetIdee: projet = new projet();
   niveaux!: Enumerations.Niveau;
-dateDebut: any;
-dateFin: any;
+  dateDebut: any;
+  dateFin: any;
   idIdeeProjet: any;
-  constructor(private route:Router,private data:GestionnaireDataService,private toast:ToastrService,private spinner:NgxSpinnerService){}
+  constructor(
+    private route: Router,
+    private data: GestionnaireDataService,
+    private toast: ToastrService,
+    private spinner: NgxSpinnerService
+  ) {}
   ngOnInit(): void {
-    if (this.data.ideeData == null || this.data.ideeData == "" || this.data.ideeData == undefined) {
-      this.route.navigate(["gestionnaire/mes_idees"]);
+    if (
+      this.data.ideeData == null ||
+      this.data.ideeData == '' ||
+      this.data.ideeData == undefined
+    ) {
+      this.route.navigate(['gestionnaire/mes_idees']);
     }
     this.projetIdee = this.data.ideeData;
     this.idIdeeProjet = this.data.ideeData.id;
-    console.log(this.niveaux)
+    console.log(this.niveaux);
   }
-  onSubmit(){
+  onSubmit() {
     const projet = {
-      titre:this.projetIdee.titre,
+      titre: this.projetIdee.titre,
       description: this.projetIdee.description,
-      estFini:false,
+      estFini: false,
       etat: true,
-      dateDebut:new Date(this.dateDebut).toISOString(),
-      dateFin:new Date(this.dateFin).toISOString(),
+      dateDebut: new Date(this.dateDebut).toISOString(),
+      dateFin: new Date(this.dateFin).toISOString(),
       niveauDAcces: this.projetIdee.niveauDAcces,
       piecesDAcces: this.projetIdee.piecesDAcces,
-      idGestionnaire: Number(localStorage.getItem("user_id")),
-      idIdeeProjet:this.idIdeeProjet,
+      idGestionnaire: Number(localStorage.getItem('user_id')),
+      idIdeeProjet: this.idIdeeProjet,
+      id: 0,
     };
 
-    console.log(projet)
+    console.log(projet);
     this.spinner.show();
     this.data.addProjet(projet).subscribe({
-
-      next:(res)=>{
+      next: (res) => {
         this.spinner.hide();
-        this.route.navigate(["gestionnaire/mes_idees"]);
+        this.route.navigate(['gestionnaire/mes_idees']);
       },
-      error:(err)=>{
+      error: (err) => {
         this.spinner.hide();
-        this.toast.error("Erreur lors de la creation du projet","Erreur")
-        console.log("Erreur est : ",err);
-      }
-    })
+        this.toast.error('Erreur lors de la creation du projet', 'Erreur');
+        console.log('Erreur est : ', err);
+      },
+    });
   }
 }
